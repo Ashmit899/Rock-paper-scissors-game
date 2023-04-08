e24c057034b05d9455b375b7082aa14a96ef7e4f
Shreya Kundu <kundushreya312@gmail.com>
2023-04-08 15:42:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const game = () => { 
+     //initialize scores of player and computer
+
+     //function to start the game
+     const startGame = () => {
+     }
+
+     //function to play match
+     const playMatch = () => {
+          //get player and computer hands with querySelector
+
+          //get available options for player-> rock, paper, scissors
+
+          //now loop through each option
+               //add event listener to each option
+               //call compareHands function with setTimeout
+               //update hand images
+     }
+
+     //function to update score
+     const updateScore = () => {
+     }
+
+     //function to compare hands
+     const compareHands = () => {
+          //update text to show who won
+
+          //check for a tie 
+
+          //check for a rock and update score, heading
+
+          //check for a paper and update score, heading
+
+          //check for scissors and update score, heading
+     }
+
+     //call all inner functions
+
+
+}
+
+game();
